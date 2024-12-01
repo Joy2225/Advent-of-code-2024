2fdc1a2a96b81185f8aee53f2346c95f3d8afd44
Joy2225 <archishman9892@gmail.com>
2024-12-01 18:47:27 +0530

    Day 1 python

--- Day 1/Historian_Hysteria.py
+++ Day 1/Historian_Hysteria.py
@@ -1,4 +1,24 @@
-with open("D1.txt") as f:
+# Part 1
+with open("D1.txt", "r") as f:
     data = f.read().splitlines()
     a, b = sorted([int(i.split()[0]) for i in data]), sorted([int(i.split()[1]) for i in data])
     print(sum([abs(a[i] - b[i]) for i in range(len(a))]))
+
+# Part 2
+def make_dict(data):
+    c = {}
+    for i in data:
+        if i not in c:
+            c[i] = 1
+        else:
+            c[i] += 1
+    return c
+with open("D1.txt", "r") as f:
+    data = f.read().splitlines()
+    data_0 = [int(i.split()[0]) for i in data]
+    data_1 = [int(i.split()[1]) for i in data]
+    a = make_dict(data_0)
+    b = make_dict(data_1)
+    # print(a)
+    # print(b)
+    print(sum([a[i] * i * b[i] for i in a.keys() if i in b.keys()]))
