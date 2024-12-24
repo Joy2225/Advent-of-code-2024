59529537009ba4cc0e02924b88232f1416836861
Joy2225 <archishman9892@gmail.com>
2024-12-24 11:32:34 +0530

    day 24

---
+++ Day_23/LAN_Party.py
@@ -0,0 +1,26 @@
+from itertools import combinations
+
+data = open("D23.txt").read().splitlines()
+connection, computer = set(), set()
+
+for i in data:
+    a, b = i.split('-')
+    computer.update([a,b])
+    connection.update([(a,b), (b,a)])
+
+t = []
+
+for a, b, c in combinations(computer, 3):
+    if {(a,b), (a,c), (b,c)} < connection and 't' in (a+b+c)[::2]:
+        t.append(a)
+
+print(len(t))
+#1238
+
+network = [{c} for c in computer]
+for n in network:
+    for c in computer:
+        if all((c,d) in connection for d in n): 
+            n.add(c)
+
+print(*sorted(max(network, key=len)), ',')
